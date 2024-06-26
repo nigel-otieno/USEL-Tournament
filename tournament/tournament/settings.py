e4648fff51721dd29a3285b8f2d183032ab4fbad
nigel-otieno <52874832+nigel-otieno@users.noreply.github.com>
2024-06-26 15:21:56 -0700

    tournament search bar update

--- tournament/tournament/settings.py
+++ tournament/tournament/settings.py
@@ -91,7 +91,7 @@ ROOT_URLCONF = 'tournament.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [os.path.join(BASE_DIR, 'tournament_app', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
