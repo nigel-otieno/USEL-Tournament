23fa39ec88d460d245a886811f91661b5331714b
nigel-otieno <52874832+nigel-otieno@users.noreply.github.com>
2024-07-09 22:59:17 -0700

    coomment fix

--- tournament/tournament/settings.py
+++ tournament/tournament/settings.py
@@ -89,7 +89,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'allauth.account.middleware.AccountMiddleware',  # Add this line
+    'allauth.account.middleware.AccountMiddleware',  
 
 ]
 
